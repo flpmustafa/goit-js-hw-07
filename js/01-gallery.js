8f63bbe421243229d4518f6c1cc95f6ec4da1ee0
Ilya <110407302+flpmustafa@users.noreply.github.com>
2022-12-17 13:50:39 +0200

    01 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+let markupGallery ='';
+for(let galleryItem of galleryItems ) {
+    markupGallery += `<div class="gallery__item">
+<a href="${galleryItem.original}" 
+class="gallery__link"">
+<img 
+class="gallery__image" 
+src="${galleryItem.preview}" 
+alt="${galleryItem.description}"
+data-sourse="${galleryItem.original}";
+>
+</a>
+</div>`;
+};
+galleryList.insertAdjacentHTML("beforeend", markupGallery);
+
+
+galleryList.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+    const originalImage = event.target.getAttribute('data-sourse');
+
+    const instance = basicLightbox.create(`
+    <img src="${originalImage}" width="800" height="600">
+`)
+
+instance.show();
+});
+
